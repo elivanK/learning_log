46e67036f2e52f2acf5bfcf50fac97489cc70eb2
Elivan Kook <elivanmetal@gmail.com>
2020-04-09 18:19:50 +0200

    fix error with owner user

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -39,9 +39,9 @@ def new_topic(request):
         # POST dat submitted and process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            new_entry = form.save(commit=False)
-            new_entry.topic = topic
-            new_entry.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # Display a blank or invalid form
     context = {'form': form}
