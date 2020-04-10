d377a2e2eec674255aeef070045869fa919e7304
Elivan Kook <elivanmetal@gmail.com>
2020-04-10 18:07:31 +0200

    ready for deplyment to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = 'users:login'
+
+# Activate Django-Heroku
+# Configure Django App for Heroku.
+#import django_heroku
+#django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
